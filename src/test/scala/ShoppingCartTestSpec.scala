ebc2eb286d021cd5f761e735b7a004fd65cefc8e
Babatunde Adekunle <badekunle@seamfix.com>
2019-01-17 20:50:44 +0100

    Coverage to add more items to Cart

--- src/test/scala/ShoppingCartTestSpec.scala
+++ src/test/scala/ShoppingCartTestSpec.scala
@@ -13,12 +13,23 @@ class ShoppingCartTestSpec extends WordSpec with Matchers {
       cart.basket.isEmpty shouldBe true
     }
 
-    "It shold add five Dove Items in cart" in {
+    "It should add five Dove Items in cart" in {
       val cart = new ShoppingCart(List.empty[Product])
       val items = List.fill(5)(Product("Dove Soap", 39.99))
       val newProducts = cart.addItems(items)
       newProducts.basket.size shouldBe 5
       newProducts.totalPrice shouldBe 199.95
+    }
+
+    "also add more items to cart" in {
+      val cart = new ShoppingCart(List.empty[Product])
+      val items = List.fill(5)(Product("Dove Soap", 39.99))
+      val newProducts = cart.addItems(items)
+
+      val moreItems = List.fill(3)(Product("Dove Soap", 39.99))
+      val additionProducts = newProducts.addItems(moreItems)
+      additionProducts.basket.size shouldBe 8
+      additionProducts.totalPrice shouldBe 319.92
     }
   }
 }
